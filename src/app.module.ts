71868232df7f39e9964e6a8137c3736fc9131f4f
daniel-ortiz-a <daniel.ortiz@alianzaelectrica.com>
2025-06-03 09:14:11 -0600

    fix:(appmodule)automatic_execution

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { SapInvoiceModule } from './sap-invoice/sap-invoice.module';
+import { InvoiceCronModule } from './config/invoice-cron.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ScheduleModule.forRoot(),
     SapInvoiceModule,
+    InvoiceCronModule,
   ],
   controllers: [],
   providers: [],
